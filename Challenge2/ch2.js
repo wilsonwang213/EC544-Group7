07893a39d9457f9e77d1352cb0d1304695aa40b0
wilsonwang213 <wilsonwang213@gmail.com>
2016-10-03 16:58:45 -0400

    Update ch2.js

--- Challenge2/ch2.js
+++ Challenge2/ch2.js
@@ -110,7 +110,7 @@ var sp;
 sp = new SerialPort.SerialPort(portName, portConfig);
 
 app.get('/', function(req, res){
-    res.sendfile('../../../../var/www/html/index.html');
+    res.sendfile('challenge2.html');
 });
 
 app.get('/', function(req, res){
