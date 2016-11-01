df02592d9915d863ec1ce367920764c4f9b1d6ce
nevermoreXY <xlevine@bu.edu>
2016-11-01 15:39:13 -0400

    FinalVersion-for final demo

--- Challenge5/finalcode/FinalCodeMain.ino
+++ Challenge5/finalcode/FinalCodeMain.ino
@@ -10,7 +10,7 @@ SoftwareSerial XBee(2, 3); // RX, TX
 bool startup = true; // used to ensure startup only happens once
 int startupDelay = 1000; // time to pause at each calibration step
 
-int cmnd = 49; //Command to start/stop the car remotely
+int cmnd = 48; //Command to start/stop the car remotely
 int cnt = 0;
 double sum = 0.0;
 
@@ -44,13 +44,14 @@ int modE;
 void setup()
 {
   Serial.begin(9600);
-
+  XBee.begin(9600);//
+  
   wheels.attach(11); // initialize wheel servo to Digital IO Pin #8
   esc.attach(13);// Start serial communications
   
-  pinMode(2, OUTPUT); // Set pin 2 as trigger pin
-  pinMode(3, INPUT); // Set pin 3 as monitor pin
-  digitalWrite(2, LOW); 
+  pinMode(8, OUTPUT); // Set pin 2 as trigger pin
+  pinMode(9, INPUT); // Set pin 3 as monitor pin
+  digitalWrite(8, LOW); 
   pinMode(4, OUTPUT); // Set pin 2 as trigger pin
   pinMode(5, INPUT); // Set pin 3 as monitor pin
   digitalWrite(4, LOW);//  
@@ -70,7 +71,19 @@ void setup()
 
 void loop()
 {
-  pulse_width = pulseIn(3, HIGH);
+  if (XBee.available())
+  {
+    int tmp = XBee.read();
+    if (tmp == 48 || tmp == 49)
+    {
+      cmnd = tmp;
+      Serial.print("xbee receive ");
+      Serial.println(cmnd);
+    }
+  }
+  if (cmnd == 49)
+  {
+  pulse_width = pulseIn(9, HIGH);
   pulse_width1 = pulseIn(5, HIGH);// Count how long the pulse is high in microseconds
   if(pulse_width != 0){ // If we get a reading that isn't zero, let's print it
         pulse_width = pulse_width/20 -7.4; // 10usec = 1 cm of distance for LIDAR-Lite
@@ -97,16 +110,7 @@ void loop()
 //    delay(5000);
 //  }
 
-  if (pulse_width1<30)
-  {
-    wheels.write(75);  
-  }
-  else if (pulse_width1>80)
-  {
-    wheels.write(105);  
-  }
-  else
-  {  
+
   
   for(int i = 0; i < arraysize; i++)
   {                    
@@ -120,7 +124,7 @@ void loop()
   modE = mode(rangevalue,arraysize);
   
 
-  if (modE<=40)
+  if (modE<=50)
   {
     esc.write(90);
   }
@@ -128,28 +132,55 @@ void loop()
   {
     esc.write(75);
 
-    pulse_width_difference = pulse_width - pulse_width1;
-    Input = pulse_width_difference;
+
+    if (pulse_width1>150 && pulse_width>150)
+    {
+      wheels.write(75);
+      delay(750);
+    }
+    else if (pulse_width1<50)
+    {
+      wheels.write(70);  
+      delay(500);
+    }
+//    else if (pulse_width1>80)
+//    {
+//      wheels.write(110);  
+//      delay(500);
+//    }
+    else
+    {  
+      
+
+      pulse_width_difference = pulse_width - pulse_width1;
+      Input = pulse_width_difference;
 
     
-    if (abs(Input-1.9)>=2.5)
-      PIDleft.SetTunings(Kp,Ki,Kd);
-    else
-      PIDleft.SetTunings(KKp,KKi,KKd);
+      if (abs(Input-1.9)>=2.5)
+        PIDleft.SetTunings(Kp,Ki,Kd);
+      else
+        PIDleft.SetTunings(KKp,KKi,KKd);
     
-    PIDleft.Compute();
-    Serial.print("input:");
-    Serial.println(Input);   
-    Serial.print("output:");
-    Serial.println(Output);
-    wheels.write(91.5+Output);
-    //wheels.write(60);
-    //wheels.write(80+Output);
-    //XBee.print(80+Output);
-  }  
+      PIDleft.Compute();
+      Serial.print("input:");
+      Serial.println(Input);   
+      Serial.print("output:");
+      Serial.println(Output);
+      wheels.write(91+Output);
+      //wheels.write(60);
+      //wheels.write(80+Output);
+      //XBee.print(80+Output);
+    }  
 
   }
+
+  } //for cmnd if
+  else
+  {
+    esc.write(90);
+  }
   // delay(1000); //Delay so we don't overload the serial port
+  
 }
 
 
