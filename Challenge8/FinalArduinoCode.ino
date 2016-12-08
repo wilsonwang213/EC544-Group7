d2420a4ce059e63c6eb5648b03745e072598bd08
nevermoreXY <xlevine@bu.edu>
2016-12-08 02:06:49 -0500

    Update FinalArduinoCode.ino

--- Challenge8/FinalArduinoCode.ino
+++ Challenge8/FinalArduinoCode.ino
@@ -1,32 +1,39 @@
+//written by zzZ, gan te me de yixiangtian!
 #include <Time.h>
 #include <PID_v1.h>
 #include <Servo.h>
-#include<SoftwareSerial.h>
-//version 2
+#include <SoftwareSerial.h>
+//version 5
 
 Servo wheels; // servo for turning the wheels
 Servo esc; // not actually a servo, but controlled like one!
 SoftwareSerial XBee(2, 3); // RX, TX
+
+unsigned long sec;
+unsigned long cur_sec;
 
 bool startup = true; // used to ensure startup only happens once
 int startupDelay = 1000; // time to pause at each calibration step
 
 int cmnd = 48; //Command to start/stop the car remotely
 int tmp = 54;
+//int tmp = 50;
 int cnt = 0;
+bool location = false;//whether the location is the elevator
+bool pre = false;
 double sum = 0.0;
 
 double cache = 0.0;
 
 
 double setPt, Input, Output;
-double Kp = 7.2;     // initial 2
-double Ki = 1;  // initial 0.05
-double Kd = 2;   // initial 0.5
+double Kp = 5;     // initial 2
+double Ki = 0.01;  // initial 0.05
+double Kd = 1;   // initial 0.5
 
-double KKp = 1.8;
-double KKi = 0.25;
-double KKd = 0.5;
+double KKp = 5;
+double KKi = 0.01;
+double KKd = 1;
 
 
 PID PIDleft(&Input, &Output, &setPt, Kp, Ki, Kd, DIRECT);
@@ -73,37 +80,64 @@ void setup()
 
 void loop()
 {
+//  sec = millis();
+//  delay(1000);
+//  cur_sec = millis();
+//  Serial.println(cur_sec-sec);
+//  delay(5000);
+//  pulse_widthright = pulseIn(9, HIGH);
+//  pulse_widthleft = pulseIn(5, HIGH);// Count how long the pulse is high in microseconds
+//  if(pulse_widthleft != 0){ // If we get a reading that isn't zero, let's print it
+//        pulse_widthleft = pulse_widthleft/20; // 10usec = 1 cm of distance for LIDAR-Lite
+//    Serial.print("pulse left: ");
+//    Serial.println(pulse_widthleft); // Print the distance
+//  }
+//   // Count how long the pulse is high in microseconds
+//  if(pulse_widthright != 0){ // If we get a reading that isn't zero, let's print it
+//       pulse_widthright = pulse_widthright/20; // 10usec = 1 cm of distance for LIDAR-Lite
+//    Serial.print("pulse right: ");
+//    Serial.println(pulse_widthright); // Print the distance
+//  }
+//  modE = pulseIn(pwPin, HIGH);
+//  modE = modE/58;
+//  if (modE<0) modE = 100;
+//  Serial.println(modE);
+//  delay(5000);
   
   if (XBee.available())    //48 for auto-drive, 49 for web-control, 50-54:forward,back,left,right,stay 
-  {
+  {                        // 55 for elevator location
     tmp = XBee.read();
     Serial.println(tmp);
     if (tmp == 48)
       cmnd = 48;
     else if (tmp == 49)
       cmnd = 49; 
+//    Serial.print("cmnd");
+//    Serial.println(cmnd);
+//    Serial.print("tmp");
+//    Serial.println(tmp);
   }
   if (cmnd == 49)
   {
      if (tmp == 50)
      {
         wheels.write(91);
-        esc.write(70);
+        esc.write(75);
      }
      else if (tmp  == 51)
      {
         wheels.write(91);
-        esc.write(110);
+        esc.write(105);
      }
      else if (tmp == 52)
      {
-        wheels.write(120);
-        esc.write(70);
+        wheels.write(135);
+        //esc.write(70);
      }
      else if (tmp == 53)
      {
-        wheels.write(60);
-        esc.write(70);
+        wheels.write(45);
+        //esc.write(70);
      }
      else if (tmp == 54)
      {
@@ -161,7 +195,7 @@ void loop()
     {
       esc.write(90);
     }
-    else if (pulse_widthright<60)
+    else if (pulse_widthright<40)
     {
       wheels.write(91);
       esc.write(115);
@@ -172,7 +206,7 @@ void loop()
       wheels.write(65);
       delay(500);
     }
-    else if (pulse_widthleft<50)
+    else if (pulse_widthleft<30)
     {
       wheels.write(135);
       esc.write(105);
@@ -184,7 +218,7 @@ void loop()
   }
   else
   {
-    esc.write(60);
+    esc.write(50);
 //
 //
 //    if (pulse_widthfront>150 && pulse_width<100)
@@ -195,9 +229,42 @@ void loop()
 
       if (pulse_widthleft>150) // && pulse_widthright<150)
       {
+        cur_sec = millis();
+        if (sec==NULL || (cur_sec-sec)>10000)
+        {
+        esc.write(90);
+        wheels.write(50);
+        esc.write(110);
+        delay(1500);
+        
+        esc.write(90);
+        wheels.write(145);
         esc.write(75);
-        wheels.write(175);
-        //delay(1000);
+        delay(4500);
+        wheels.write(91);
+        delay(3000);
+//        esc.write(90);
+//        delay(1000000000);
+        sec = cur_sec;
+        }
+        else
+        {
+          wheels.write(91);
+          esc.write(70);
+          delay(3000);
+//          esc.write(90);
+//          delay(1000000);
+        }
+        
+
+//        esc.write(90);
+//        delay(500);
+//        wheels.write(90.5);
+//        esc.write(50);
+//        delay(2500);
+//        esc.write(90);
+//        delay(1000000000);
+        
       }
       else if (pulse_widthleft<30)
       {
@@ -214,6 +281,8 @@ void loop()
       else
       {
         //esc.write(75);
+      if (pulse_widthleft<pulse_widthright)
+      {
       pulse_width_difference = 50 - pulse_widthleft;
       Input = pulse_width_difference;
 
@@ -232,12 +301,33 @@ void loop()
       //wheels.write(60);
       //wheels.write(80+Output);
       //XBee.print(80+Output);
+      }//left or right
+      else
+      {
+        pulse_width_difference = 60 - pulse_widthright;
+      Input = pulse_width_difference;
+
+    
+      if (abs(Input)>=2.5)
+        PIDleft.SetTunings(Kp,Ki,Kd);
+      else
+        PIDleft.SetTunings(KKp,KKi,KKd);
+    
+      PIDleft.Compute();
+      Serial.print("input:");
+      Serial.println(Input);   
+      Serial.print("output:");
+      Serial.println(Output);
+      wheels.write(91-Output);
       }
-
+      } 
+      
+     
+     
   }
 
   }
-  
+ 
 }
 
 
